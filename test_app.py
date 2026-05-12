9036e4faa694bcadfa0065c974cc43e91ac197ca
vamsisaikrishna-m <vamsisaikrishna.m@centific.com>
2026-05-12 20:03:24 +0530

    first commit

---
+++ test_app.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from cidcd_demo.app import app
+
+client = TestClient(app)
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+
+def test_add():
+    response = client.get("/add?a=2&b=3")
+    assert response.json() == {"result": 5}
